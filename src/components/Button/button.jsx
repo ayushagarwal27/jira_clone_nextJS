27b80b25b359bc7965732d1bf9e0988166501c57
Ayush <ayushagarwal.dev@gmail.com>
2024-04-19 21:45:36 +0530

    fix button test

--- src/components/Button/button.jsx
+++ src/components/Button/button.jsx
@@ -1,18 +1,6 @@
 import React from "react";
 
 export const Button = ({ onClick }) => {
-  if (!onClick) {
-    return (
-      <button
-        onClick={() => {
-          console.log("Hello");
-        }}
-        data-testid="button-test"
-      >
-        button
-      </button>
-    );
-  }
   return (
     <button onClick={onClick} data-testid="button-test">
       button
