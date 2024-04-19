e60f5b9c61858794af067b0ae2f4753d69398316
Ayush <ayushagarwal.dev@gmail.com>
2024-04-19 21:31:48 +0530

    evil-commit

--- src/components/Button/button.jsx
+++ src/components/Button/button.jsx
@@ -1,6 +1,18 @@
 import React from "react";
 
 export const Button = ({ onClick }) => {
+  if (!onClick) {
+    return (
+      <button
+        onClick={() => {
+          console.log("Hello");
+        }}
+        data-testid="button-test"
+      >
+        button
+      </button>
+    );
+  }
   return (
     <button onClick={onClick} data-testid="button-test">
       button
