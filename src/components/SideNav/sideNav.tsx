5b53ad3f1539a570a34de2bde0d4f0136a9d8248
Ayush <ayushagarwal.dev@gmail.com>
2024-05-12 21:48:48 +0530

    add sidebar-nav-item

--- src/components/SideNav/sideNav.tsx
+++ src/components/SideNav/sideNav.tsx
@@ -1,11 +1,27 @@
 import { getAllBoards } from "@/app/actions/board";
+import { SideNavItem } from "@/components/SideNav/sideNavItem";
+import { SideNavSeparator } from "@/components/SideNav/sideNavSeparator";
+import { auth } from "@/lib/auth";
 
 export const SideNav = async () => {
+  const session = await auth();
+
+  if (!session) {
+    return <></>;
+  }
+
   const boardsData = await getAllBoards();
   return (
-    <div className={"fixed w-full sm:w-[250px] bg-amber-200"}>
-      {boardsData.map((board) => (
-        <p>{board.title}</p>
+    <div
+      className={
+        "fixed w-full sm:w-[250px]  sidebar-bg h-[calc(100vh-60px)] mt-[60px]"
+      }
+    >
+      {boardsData.map(({ id, title }) => (
+        <>
+          <SideNavItem id={id} title={title} key={id} />
+          <SideNavSeparator />
+        </>
       ))}
     </div>
   );
