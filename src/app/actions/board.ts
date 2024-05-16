7af13652636ecb1002773443a91a52be2011f7cb
Ayush <ayushagarwal.dev@gmail.com>
2024-05-16 20:16:19 +0530

    add server action for board page data

--- src/app/actions/board.ts
+++ src/app/actions/board.ts
@@ -1,10 +1,36 @@
 "use server";
 
 import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
 
 export async function getAllBoards() {
   const boards = await prisma.board.findMany({
     select: { id: true, title: true },
   });
   return boards;
+}
+
+export async function fetchBoard(boardId: string, skip = 0, take = 10) {
+  if (!boardId) {
+    throw Error("Fields are missing");
+  }
+  const board = prisma.board.findUniqueOrThrow({
+    where: { id: boardId },
+  });
+
+  const boardColumns = prisma.boardColumn.findMany({
+    where: { boardId },
+  });
+
+  const boardTickets = prisma.boardTicket.findMany({
+    where: { boardId },
+    skip: skip,
+    take: take,
+  });
+
+  const data = await Promise.all([board, boardColumns, boardTickets]);
+
+  return {
+    data: { board: data[0], boardColumns: data[1], boardTickets: data[2] },
+  };
 }
--- src/app/board/[id]/page.tsx
+++ src/app/board/[id]/page.tsx
@@ -1,5 +1,13 @@
-const BoardPage = () => {
-  return <div className={"pt-16 sm:ml-[250px]"}>sdds</div>;
+import { FC } from "react";
+import { fetchBoard } from "@/app/actions/board";
+
+interface BoardPageProps {
+  params: { id: string };
+}
+
+const BoardPage: FC<BoardPageProps> = async ({ params: { id: boardId } }) => {
+  const { data } = await fetchBoard(boardId);
+  return <div className={"pt-16 sm:ml-[250px]"}>{boardId}</div>;
 };
 
 export default BoardPage;
