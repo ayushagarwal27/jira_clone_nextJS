91db6d7cd0d82d1022d43aca0b279339855ae88a
Ayush <ayushagarwal.dev@gmail.com>
2024-05-18 23:46:14 +0530

    Add drag and drop feature client side

--- src/components/Board/board.tsx
+++ src/components/Board/board.tsx
@@ -1,6 +1,14 @@
-import React, { FC } from "react";
+"use client";
+
+import React, { FC, useState } from "react";
 import { BoardColumn, BoardTicket } from "@prisma/client";
 import { Ticket } from "@/components/Board/Ticket";
+import {
+  DragDropContext,
+  Droppable,
+  OnDragEndResponder,
+} from "@hello-pangea/dnd";
+import cx from "classnames";
 
 interface BoardTicketWithUser extends BoardTicket {
   assignedUser: { name: string };
@@ -12,32 +20,81 @@ interface BoardProps {
 }
 
 const Board: FC<BoardProps> = ({ boardTickets, boardColumns }) => {
+  const [tickets, setTickets] = useState(boardTickets);
+
   const getTicketForColumn = (columnId: string) => {
-    return boardTickets.filter((ticket) => ticket.boardColumnId === columnId);
+    return tickets
+      .filter((ticket) => ticket.boardColumnId === columnId)
+      .sort((a, b) => a.position - b.position);
   };
+
+  const handleDragEnd: OnDragEndResponder = (result) => {
+    const currentTickets = [...tickets];
+    const updatedTickets = currentTickets.map((ticket: BoardTicketWithUser) => {
+      if (ticket.id === result.draggableId) {
+        return {
+          ...ticket,
+          boardColumnId: result.destination?.droppableId,
+          position: result.destination?.index,
+        };
+      } else if (
+        ticket.boardColumnId === result.destination?.droppableId &&
+        ticket.position >= result.destination?.index
+      ) {
+        return {
+          ...ticket,
+          position: ticket.position + 1,
+        };
+      } else if (
+        ticket.boardColumnId === result.source.droppableId &&
+        ticket.position > result.source.index
+      ) {
+        return {
+          ...ticket,
+          position: ticket.position - 1,
+        };
+      }
+      return ticket;
+    });
+
+    // @ts-ignore
+    setTickets(updatedTickets);
+  };
+
   return (
-    <div className={"flex flex-col sm:flex-row gap-3"}>
-      {boardColumns.map((col) => (
-        <div
-          key={col.id}
-          className={"background-neutral-static grow basis-full p-2"}
-        >
-          <div className="text-subtlest font-semibold text-sm mb-5">
-            {col.label}
-          </div>
-          <div>
-            {getTicketForColumn(col.id).map((ticket) => (
-              <Ticket
-                key={ticket.id}
-                title={ticket.title}
-                assignee={ticket.assignedUser.name}
-                storyPoints={ticket.storyPoints}
-              />
-            ))}
-          </div>
-        </div>
-      ))}{" "}
-    </div>
+    <DragDropContext onDragEnd={handleDragEnd}>
+      <div className={"flex flex-col sm:flex-row gap-3"}>
+        {boardColumns.map((col) => (
+          <Droppable key={col.id} droppableId={col.id}>
+            {(provided, snapshot) => (
+              <div
+                ref={provided.innerRef}
+                className={cx("background-neutral-static grow basis-full p-2", {
+                  ["background-board-drop"]: snapshot.isDraggingOver,
+                })}
+                {...provided.droppableProps}
+              >
+                <div className="text-subtlest font-semibold text-sm mb-5">
+                  {col.label}
+                </div>
+                <div>
+                  {getTicketForColumn(col.id).map((ticket, index) => (
+                    <Ticket
+                      key={ticket.id}
+                      index={index}
+                      ticketId={ticket.id}
+                      title={ticket.title}
+                      assignee={ticket.assignedUser.name}
+                      storyPoints={ticket.storyPoints}
+                    />
+                  ))}
+                </div>
+              </div>
+            )}
+          </Droppable>
+        ))}
+      </div>
+    </DragDropContext>
   );
 };
 
