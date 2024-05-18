195a2c0d1d457581edad4a25ed65be99801e79d1
Ayush <ayushagarwal.dev@gmail.com>
2024-05-18 23:48:11 +0530

    Add drag and drop feature client side (#20)

--- src/components/Board/Ticket.tsx
+++ src/components/Board/Ticket.tsx
@@ -1,26 +1,45 @@
+"use client";
 import React, { FC } from "react";
+import { Draggable } from "@hello-pangea/dnd";
 
 interface TicketProps {
   title: string;
+  index: number;
   assignee: string;
   storyPoints: number | null;
+  ticketId: string;
 }
 
-const Ticket: FC<TicketProps> = ({ title, assignee, storyPoints }) => {
+const Ticket: FC<TicketProps> = ({
+  ticketId,
+  index,
+  title,
+  assignee,
+  storyPoints,
+}) => {
   return (
-    <div className="m-1 h-[200px] background-card p-3 rounded cursor-pointer relative">
-      <div>{title}</div>
-      {assignee && (
-        <div className="absolute top-2 right-2 rounded-full background-avatar text-white h-10 w-10 text-sm flex items-center justify-center font-semibold">
-          {assignee[0]}
+    <Draggable draggableId={ticketId} index={index}>
+      {(provided, snapshot) => (
+        <div
+          ref={provided.innerRef}
+          className="m-1 h-[200px] background-card p-3 rounded cursor-pointer relative"
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+        >
+          <div>{title}</div>
+          {assignee && (
+            <div className="absolute top-2 right-2 rounded-full background-avatar text-white h-10 w-10 text-sm flex items-center justify-center font-semibold">
+              {assignee[0]}
+            </div>
+          )}
+          {storyPoints && (
+            <div className="absolute bottom-2 left-2 rounded-full background-box text-white h-7 w-7 text-sm flex items-center justify-center">
+              {storyPoints}
+            </div>
+          )}
         </div>
       )}
-      {storyPoints && (
-        <div className="absolute bottom-2 left-2 rounded-full background-box text-white h-7 w-7 text-sm flex items-center justify-center">
-          {storyPoints}
-        </div>
-      )}
-    </div>
+    </Draggable>
   );
 };
 
